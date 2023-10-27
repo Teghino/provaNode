9d584448decd7e0166199b75f566edfad226ef76
Teghino <tacchifra@gmail.com>
2023-10-27 10:29:59 +0200

    finito login

--- router/utenti.js
+++ router/utenti.js
@@ -13,16 +13,6 @@ const { exit } = require('process');
 //router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(express.static('public'));
-
-router.post('/login', (req, res) => {
-    const dati = req.body;
-    User.findAll().then((users) => {
-        console.log('Elenco di utenti:', users);
-      }).catch((error) => {
-        console.error('Errore:', error);
-      });
-        // Chiudi la connessione al database quando hai finito
-});
   
 router.post('/register',  (req, res) => {
     const dati = req.body;
@@ -49,21 +39,21 @@ router.post('/login', (req, res) => {
       email: dati.username,
       psw: dati.password,
     },
-    
   })
-    .then((users) => {
-      console.log('Elenco di utenti:', users);
-    }).catch((error) => {
-      console.error('Errore:', error);
-    });
+    .then((utente) => {
+      if (utente == null) {
+        res.sendFile('accessofallito.html', { root: __dirname + '/../public' });
+      } else {
+        console.log(utente);
+        res.status(200).send('Accesso effettuato correttamente');
+      }
       // Chiudi la connessione al database quando hai finito
+    })
+    .catch((error) => {
+      console.error('Errore:', error);
+      res.status(500).send('Internal Server Error');
+      // Chiudi la connessione al database quando hai finito
+    });
 });
-
-
-// Esempio: seleziona tutti gli utenti dalla tabella Utenti
-
-  
-
-  
 
 module.exports = router;
